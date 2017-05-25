fa735f28ed37b584e725595ba64dc24e751e7753
thebrianemory <the@brianemory.com>
2017-05-24 22:07:43 -0700

    Add plug to browser pipeline

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Vidshare.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Vidshare.Plugs.SetUser
   end
 
   pipeline :api do
