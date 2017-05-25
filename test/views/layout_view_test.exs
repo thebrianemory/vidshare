a5ca793ff8f3d223343cff21092f5db32a593313
thebrianemory <the@brianemory.com>
2017-05-24 21:28:12 -0700

    Create test for navbar

--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -1,3 +1,8 @@
 defmodule Vidshare.LayoutViewTest do
   use Vidshare.ConnCase, async: true
+
+  test "Verify navbar is displayed", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "<div class=\"top-bar\" id=\"my-menu\">"
+  end
 end
