dbab28b8529aa381b4e787b3329e92130765ea35
thebrianemory <the@brianemory.com>
2017-05-24 22:05:48 -0700

    Add test for signing in with Google

--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -5,4 +5,20 @@ defmodule Vidshare.LayoutViewTest do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "<nav class=\"navbar navbar-default\">"
   end
+
+  test "Sign in with Google link shown when not signed in", %{conn: conn} do
+    conn = get conn, "/"
+
+    assert html_response(conn, 200) =~ "Sign in with Google"
+  end
+
+  test "Sign out link shown when signed in", %{conn: conn} do
+    user = insert(:user)
+
+    conn = conn
+    |> assign(:user, user)
+    |> get("/")
+
+    assert html_response(conn, 200) =~ "Sign out"
+  end
 end
