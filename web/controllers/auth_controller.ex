7b1924dd6e53a978df104f1e18a89aebdf595d72
thebrianemory <the@brianemory.com>
2017-05-24 22:03:40 -0700

    Add code for creating a user with ueberauth

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,4 +1,36 @@
 defmodule Vidshare.AuthController do
   use Vidshare.Web, :controller
   plug Ueberauth
+
+  alias Vidshare.User
+
+  def new(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{token: auth.credentials.token, first_name: auth.info.first_name, last_name: auth.info.last_name, email: auth.info.email, provider: "google"}
+    changeset = User.changeset(%User{}, user_params)
+
+    create(conn, changeset)
+  end
+
+  def create(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Thank you for signing in!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: "/")
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect("/")
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+  end
 end
