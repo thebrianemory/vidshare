6fe77f6425b59e4686820a0bc28bd1a3abcd714a
thebrianemory <the@brianemory.com>
2017-05-24 23:17:56 -0700

    Update test

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -7,7 +7,7 @@ defmodule Vidshare.VideoControllerTest do
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, video_path(conn, :index)
-    assert html_response(conn, 200) =~ "All videos"
+    assert html_response(conn, 200) =~ "Sort by:"
   end
 
   test "renders form for new resources", %{conn: conn} do
