856732ef47c34b80a919bbd194454761165adfc8
thebrianemory <the@brianemory.com>
2017-05-24 22:02:17 -0700

    Add user model

---
+++ web/models/user.ex
@@ -0,0 +1,19 @@
+defmodule Vidshare.User do
+  use Vidshare.Web, :model
+
+  schema "users" do
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :provider, :string
+    field :token, :string
+
+    timestamps()
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:first_name, :last_name, :email, :provider, :token])
+    |> validate_required([:first_name, :last_name, :email, :provider, :token])
+  end
+end
