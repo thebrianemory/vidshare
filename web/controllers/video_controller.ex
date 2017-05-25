f54b08ce50ad768037e56343015c211398a07304
thebrianemory <the@brianemory.com>
2017-05-24 22:37:28 -0700

    Update create action

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -14,15 +14,55 @@ defmodule Vidshare.VideoController do
   end
 
   def create(conn, %{"video" => video_params}) do
-    changeset = Video.changeset(%Video{}, video_params)
+    # Sets regex to nil if invalid URL to make sure we only get valid YouTube links
+    regex = Regex.run(~r/(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/, video_params["video_id"])
 
-    case Repo.insert(changeset) do
-      {:ok, _video} ->
-        conn
-        |> put_flash(:info, "Video created successfully.")
-        |> redirect(to: video_path(conn, :index))
-      {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset)
+    if regex == nil do
+      changeset = Video.changeset(%Video{})
+
+      conn
+      |> put_flash(:error, "Invalid YouTube URL")
+      |> render("new.html", changeset: changeset)
+    else
+      # Grab only the video ID from the submitted YouTube link
+      video_id = List.first(tl(regex))
+
+      # Submit our info to the YouTube API and get back the JSON
+      json_data = HTTPoison.get! "https://www.googleapis.com/youtube/v3/videos?id=#{video_id}&key=#{System.get_env("YOUTUBE_API_KEY")}&part=snippet,statistics,contentDetails&fields=items(id,snippet(title,thumbnails(high)),statistics(viewCount),contentDetails(duration))"
+
+      # Decode the JSON
+      data = Poison.decode!(json_data.body, keys: :atoms)
+
+      # Grab our items from the JSON list within our data
+      items = hd(data.items)
+
+      # Convert the duration into a human readable format
+      length_regex = tl(Regex.run(~r/PT(\d+H)?(\d+M)?(\d+S)?/, items.contentDetails.duration))
+      duration = get_formatted_time(length_regex)
+
+      # The information we need to create our video
+      valid_attrs = %{duration: duration, thumbnail: items.snippet.thumbnails.high.url,
+                      title: items.snippet.title, video_id: items.id,
+                      view_count: String.to_integer(items.statistics.viewCount)}
+
+      # Creates are changeset and builds the association with the current user
+      changeset = conn.assigns.user
+      |> build_assoc(:videos)
+      |> Video.changeset(valid_attrs)
+
+      # Creates our video in the database if it does not already exist
+      # If it fails to create at this point, that means it already exists. Redirect to its show page
+      case Repo.insert(changeset) do
+        {:ok, video} ->
+          conn
+          |> put_flash(:info, "Video created successfully.")
+          |> redirect(to: video_path(conn, :show, video))
+        {:error, _video} ->
+          video = Vidshare.Video |> Vidshare.Repo.get_by(video_id: video_id)
+          conn
+          |> put_flash(:info, "Video has already been created.")
+          |> redirect(to: video_path(conn, :show, video))
+      end
     end
   end
 
